bf1277f1693b0568fc1d33b6e71c0a2121b018e6
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-01-02 18:47:39 +0100

    Find super stacks fun

--- R/parse-grid.R
+++ R/parse-grid.R
@@ -29,7 +29,46 @@ check_stacks <- function(grid_matrix) {
     stacks_by_col$group_size <- NULL
 
     stack_rows <- rbind(stack_rows, stacks_by_col)
+
+    stack_rows <- stack_rows[!duplicated(stack_rows[,c('row_start','row_end')]),]
   }
 
   return(stack_rows)
+}
+
+# Both type of stacks need an individual fluidRow()
+# 1. super stacks
+# 2. stacks
+
+check_super_stacks <- function(stack_rows){
+
+  super_stacks <- data.frame()
+
+  for(row in seq_len(nrow(stack_rows))){
+
+    for(i in seq_len(nrow(stack_rows))){
+
+      if(stack_rows$stacked_elements[row] != stack_rows$stacked_elements[i]){
+
+        if(stack_rows$row_start[row] >= stack_rows$row_start[i] &
+           stack_rows$row_start[row] <= stack_rows$row_end[i]){
+
+          confirmed_super_stacks <- data.frame(
+            stacked_elements = paste0(
+              stack_rows$stacked_elements[row], "-", stack_rows$stacked_elements[i]),
+            row_start = min(stack_rows$row_start[row], stack_rows$row_start[i]),
+            row_end = max(stack_rows$row_end[row], stack_rows$row_end[i])
+          )
+
+          super_stacks <- rbind(super_stacks, confirmed_super_stacks)
+
+          super_stacks <- super_stacks[!duplicated(super_stacks[,c('row_start','row_end')]),]
+
+        }
+      }
+    }
+
+  }
+
+  return(super_stacks)
 }
