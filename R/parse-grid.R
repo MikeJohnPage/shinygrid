a660d7b1183821f5a5c2b721b9eb190c47d8dde3
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-01-03 18:53:46 +0100

    Iterate over super stacks. Add single rows to identify all fluidRows(). Add edge case examples.

--- R/parse-grid.R
+++ R/parse-grid.R
@@ -27,22 +27,22 @@ check_stacks <- function(grid_matrix) {
 
     stacks_by_col <- groups_by_col[groups_by_col$group_size > 1, ]
     stacks_by_col$group_size <- NULL
-
     stack_rows <- rbind(stack_rows, stacks_by_col)
-
     stack_rows <- stack_rows[!duplicated(stack_rows[,c('row_start','row_end')]),]
+
   }
 
   return(stack_rows)
 }
 
-# Both type of stacks need an individual fluidRow()
-# 1. super stacks
-# 2. stacks
-
+# ---- Check for super stacks (stacked stacks) ----
 check_super_stacks <- function(stack_rows){
 
+  stack_rows$id <- seq_along(stack_rows[,1])
+
   super_stacks <- data.frame()
+
+  super_stack_tracker <- vector()
 
   for(row in seq_len(nrow(stack_rows))){
 
@@ -59,44 +59,125 @@ check_super_stacks <- function(stack_rows){
             row_start = min(stack_rows$row_start[row], stack_rows$row_start[i]),
             row_end = max(stack_rows$row_end[row], stack_rows$row_end[i])
           )
-
           super_stacks <- rbind(super_stacks, confirmed_super_stacks)
-
           super_stacks <- super_stacks[!duplicated(super_stacks[,c('row_start','row_end')]),]
-
+          super_stack_tracker <- c(super_stack_tracker, stack_rows$id[row], stack_rows$id[i])
         }
       }
     }
-
   }
 
-  return(super_stacks)
+  not_a_super_stack_ids <- setdiff(stack_rows$id, unique(super_stack_tracker))
+  not_a_super_stack_df <- subset(stack_rows, id %in% not_a_super_stack_ids)
+  not_a_super_stack_df$id <- NULL
+
+  complete_stack <- rbind(super_stacks, not_a_super_stack_df)
+
+  return(complete_stack)
+}
+
+# ---- Iterate over super stacks ----
+iterate_stacks <- function(stack){
+
+  current_stack <- check_super_stacks(stack)
+  current_stack_size <- nrow(current_stack)
+
+  while(nrow(check_super_stacks(current_stack)) < current_stack_size){
+    current_stack <- check_super_stacks(current_stack)
+    current_stack_size <- nrow(current_stack)
+  }
+
+  return(current_stack)
+}
+
+# ---- Rows covered by stacks ----
+rows_covered_by_stacks <- function(stack){
+
+  rows_covered <- vector()
+
+  for(row in seq_len(nrow(stack))){
+    rows_covered <- c(rows_covered, stack$row_start[row]:stack$row_end[row])
+  }
+
+  return(rows_covered)
+}
+
+rows_not_covered_by_stacks <- function(rows_covered, grid_matrix){
+
+  rows_not_covered <- setdiff(1:nrow(grid_matrix), rows_covered)
+
+}
+
+# ---- append single rows ----
+append_single_rows <- function(rows_not_covered, current_stack) {
+
+  single_rows_df <- data.frame()
+
+  for(i in seq_along(rows_not_covered)){
+    single_rows <- data.frame(
+      stacked_elements = "No stack",
+      row_start = rows_not_covered[i],
+      row_end = rows_not_covered[i]
+    )
+
+    single_rows_df <- rbind(single_rows_df, single_rows)
+  }
+
+  all_rows <- rbind(current_stack, single_rows_df)
+
+  return(all_rows)
+
 }
 
 # ---- To develop / test ----
+# Multiple super stacks and stacks
 t <- parse_grid(
   "a b c",
   "a d e",
   "f d g",
   "h i g",
+  "y z z", # random row with no stacks to separate super stacks
   "j k l",
   "j m n",
-  "o m p"
+  "o m p",
+  "q r s",
+  "q t u",
+  "v w x" # random row with no stacks to track back
 )
 
 ts <- check_stacks(t)
 
-# problem, when you run check_super_stacks on ts, the functions needs to be run
-# again to identify to new super stacks. This process needs to be run iteratively
+its <- iterate_stacks(ts)
 
-# solution
-# At the end of check_super_stacks, just check if stacked_elements share letters,
-# if they do, combine into one super stack.
-# Whats happens if free super stacks share letters but different ones, e.g.,
-# "a-b", "b-c", and "c-d"? Perhaps when joining stacks and finding min and max
-# Row numbers, grow the stacked_elements by including all the letters, e.g.,
-# after joining "a-b", and "b-c", store that as an intermediary variable called
-# "a-b-c". This can be the matched to "c-d". The order doesn't matter.
-# Questions: duplicates are removed in check_stack(), is this going to mean
-# some stacked_elements are missed using this strategy?
+its_c <- rows_covered_by_stacks(its)
+
+its_nc <- rows_not_covered_by_stacks(rows_covered = its_c, t)
+
+# One huge super stack
+v <- parse_grid(
+  "a b c v",
+  "a d e v",
+  "f d g v",
+  "h i g v",
+  "j k l v",
+  "j m n v",
+  "o m p v",
+  "q r s v",
+  "q t u v"
+)
+
+vs <- check_stacks(v)
+
+ivs <- iterate_stacks(vs)
+
+# # No stacks (i.e., df returns zero)
+# w <- parse_grid("a b c",
+#                 "d e f")
+#
+# wa <- check_stacks(w)
+#
+# iwa <- iterate_stacks(wa)
+#
+# w_c <- rows_covered_by_stacks(iwa)
+
 
